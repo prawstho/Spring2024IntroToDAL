df7f88dcc38a0b584f9c8b522ad92f82709c50d1
Peter <113168584+prawstho@users.noreply.github.com>
2024-06-05 10:35:13 -0230

    fetching actor film data

--- services/actors.dal.js
+++ services/actors.dal.js
@@ -28,6 +28,33 @@ var getActors = function() {
   }); 
 };
 
+var getActorFilms = function() {
+  if(DEBUG) console.log("actors.dal.getActorFilms()");
+  return new Promise(function(resolve, reject) {
+
+    // const sql = "SELECT first_name, last_name, title, release_year, rating \
+    //   FROM actor \
+    //   INNER JOIN film_actor USING (actor_id) \
+    //   INNER JOIN film USING (film_id) \
+    //   ORDER BY last_name \
+    //   LIMIT 100;"
+
+    const sql = `SELECT * FROM public."vw_ActorFilms" LIMIT 60;`
+
+    pool.query(sql, [], (err, result) => {
+      if (err) {
+        if(DEBUG) console.log(err);
+        reject(err);
+      } else {
+        if(DEBUG) console.log("success got data from inside the actors.dal.getActorFilms().");
+        if(DEBUG) console.log(result.rows);
+        resolve(result.rows);
+      }
+    }); 
+  });   
+}
+
 module.exports = {
   getActors,
+  getActorFilms,
 }
